61cbcea1f4043ba1d405ccfd6db41c2e84c58451
brad <b.bradfrancis@gmail.com>
2020-07-09 22:27:45 +1000

    Added Sticky Header with Animations

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import './index.css';
+import { useSpring, animated } from 'react-spring';
+
+const smallMenuOffset = 50;
 
 const Header = () => {
+	const [scrollPosition, setSrollPosition] = useState(0);
+	const handleScroll = () => {
+		const position = window.pageYOffset;
+		setSrollPosition(position);
+	};
+	
+	useEffect(() => {
+		window.addEventListener('scroll', handleScroll, { passive: true });
+	
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
+
+	const spring = useSpring({
+		height: scrollPosition > smallMenuOffset ? '4rem' : '10rem'
+	});
+
+	const springFont = useSpring({
+		fontSize: scrollPosition > smallMenuOffset ? '2rem' : '3rem'
+	});
+
 	return (
-		<div className="header-container">
-			<h1>Odour B Gone</h1>
-			<nav>
-				<button>HOME</button>
-				<button>PRODUCTS</button>
-				<button>STOCKISTS</button>
-				<button>ABOUT</button>
-				<button>CONTACT</button>
-			</nav>
+		<div>
+			<animated.div style={spring} className="header-container">
+				<animated.h1 style={springFont}>Odour B Gone</animated.h1>
+				<nav>
+					<button>HOME</button>
+					<button>PRODUCTS</button>
+					<button>STOCKISTS</button>
+					<button>ABOUT</button>
+					<button>CONTACT</button>
+				</nav>
+			</animated.div>
+			<div className="headerImage-dummy-container">
+
+			</div>
 		</div>
 	);
 }
--- src/components/headerImage/index.js
+++ src/components/headerImage/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import './index.css';
 
 const HeaderImage = () => {
+
 	return (
-		<div class="headerImage-container">
+		<div className="headerImage-container">
 			<div className="headerImage-background">
 				<h2>RV TOILET TABLETS</h2>
 			</div>
