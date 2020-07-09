f26ac6994f10444eecfb91fc67fc24da5f1e3f65
brad <b.bradfrancis@gmail.com>
2020-07-09 21:58:46 +1000

    Added Basic Header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './styles/baseStyles.css';
 import Header from './components/header';
 import Products from './components/products';
+import HeaderImage from './components/headerImage';
 
 const App = () => {
   return (
     <div>
-		  <Header />
-		  <Products />
+		<Header />
+		<HeaderImage />
+		<Products />
     </div>
   );
 }
---
+++ src/components/headerImage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const HeaderImage = () => {
+	return (
+		<div class="headerImage-container">
+			<div className="headerImage-background">
+				<h2>RV TOILET TABLETS</h2>
+			</div>
+		</div>
+	)
+}
+
+export default HeaderImage;
