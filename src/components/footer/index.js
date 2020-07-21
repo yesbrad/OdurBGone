5521aa360361c74951d793e450aab392955b6d5f
brad <b.bradfrancis@gmail.com>
2020-07-21 23:09:30 +1000

    Footer Pass

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -11,6 +11,7 @@ const Footer = () => {
 				<button>ABOUT</button>
 				<button>CONTACT</button>
 				<h3>2020 &#169; Odour-B-Gone</h3>
+				<h2 onClick={() => window.open('http://bradfrancis.com.au')}>Designed & Developed by bradfrancis.com.au</h2>
 			</div>
 		</div>
 	)
