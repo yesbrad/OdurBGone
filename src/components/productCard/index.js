332e1744519083fcc2e883f6b1fc53f9e1096866
brad <b.bradfrancis@gmail.com>
2020-07-28 23:34:07 +1000

    Product Card Pass

--- src/components/productCard/index.js
+++ src/components/productCard/index.js
@@ -3,18 +3,6 @@ import './index.css';
 import { useSpring, animated } from 'react-spring';
 import { FaChevronUp } from 'react-icons/fa';
 
-const description = `CASSETTE TOILETS : Simply drop 1 x 8 gram Blue tablet into the bottom cassette with half cup water, lasting up to 4 Days usage. The tablet will break down the matter including the paper very quickly. There are 2 size pouches, 50 x 8gram tablets (blue) & 25 x 8gram tablets both include 10 x 2gram. Mini oxygen tabs for use in a spray bottle.
-The 2 gram Mini Tab (green) is used in a spray bottle, simply drop into a 400 mil bottle with cup hot water & top up with cold water, give it a shake and is ready for use. We use the spray bottle for the bowl & seat, which only takes seconds & in the heat perhaps spray the bowl 3 times a day. By spraying it is killing the bad bacteria & any odour, it is good to open the slide when spraying to allow entry into the bottom cassette.
-
-Use any toilet paper & if it is not breaking down use more flushing water.
-
-HOLDING TANKS: - Depending on the size of the tank I recommend using 1 x 30 gram tablet (white) for 7 days usage , add more as required. The tablet is similar to the 8 gram but more economical for such tanks.If you have a boat/yacht etc sea water can be used and with a Masurator suggest crush the tablet install pieces to prevent from jamming. If you have the small bottle Mini tabs enclosed in your packaging I suggest use the same method as shown above for the bowl & seat.
-
-The Mini Oxygen Tab in a spray bottle lasts around 3 weeks can be used in the kitchen, Bathroom on bench tops etc and has NO perfume.
-
-GREY WATER: - I recommend using the 30 gram tablet can be placed on the far corner of theshower base , this will clean out the pipe lines leading to the tank, also can be placed directly intothe tank.
-`;
-
 const ProductCard = ({ product }) => {
 	const [isOpen, setIsOpen] = useState(false);
 
@@ -47,7 +35,7 @@ const ProductCard = ({ product }) => {
 					<animated.h2 style={headerProps}>{product.name}</animated.h2>
 					<div className="product-desctiption-container">
 						<animated.span style={shortDescriptionProps}>{product.shortDescription}</animated.span>
-						<animated.div className="product-description-paragraph" style={descriptionProps}>{description.split('\n').map((item, i) => {
+						<animated.div className="product-description-paragraph" style={descriptionProps}>{product.description.split('\n').map((item, i) => {
 							return <span key={i}>{item}</span>;
 						})}</animated.div>
 					</div>
--- src/data/products/index.js
+++ src/data/products/index.js
@@ -2,12 +2,7 @@ const data = [
 	{
 		name: '20x8g Cassette Toilet Tablets Pouch',
 		shortDescription: 'With 10x2g Mini Oxygen Tablets',
-		description: `CASSETTE TOILETS : Simply drop 1 x 8 gram Blue tablet into the bottom cassette with half cup water, lasting up to 4 Days usage. The tablet will break down the matter including the paper very quickly. There are 2 size pouches, 50 x 8gram tablets (blue) & 25 x 8gram tablets both include 10 x 2gram. Mini oxygen tabs for use in a spray bottle.
-		The 2 gram Mini Tab (green) is used in a spray bottle, simply drop into a 400 mil bottle with cup hot water & top up with cold water, give it a shake and is ready for use. We use the spray bottle for the bowl & seat, which only takes seconds & in the heat perhaps spray the bowl 3 times a day. By spraying it is killing the bad bacteria & any odour, it is good to open the slide when spraying to allow entry into the bottom cassette.
-
-		Use any toilet paper & if it is not breaking down use more flushing water.
-
-		HOLDING TANKS: - Depending on the size of the tank I recommend using 1 x 30 gram tablet (white) for 7 days usage , add more as required. The tablet is similar to the 8 gram but more economical for such tanks.If you have a boat/yacht etc sea water can be used and with a Masurator suggest crush the tablet install pieces to prevent from jamming. If you have the small bottle Mini tabs enclosed in your packaging I suggest use the same method as shown above for the bowl & seat.
+		description: `HOLDING TANKS: - Depending on the size of the tank I recommend using 1 x 30 gram tablet (white) for 7 days usage , add more as required. The tablet is similar to the 8 gram but more economical for such tanks.If you have a boat/yacht etc sea water can be used and with a Masurator suggest crush the tablet install pieces to prevent from jamming. If you have the small bottle Mini tabs enclosed in your packaging I suggest use the same method as shown above for the bowl & seat.
 
 The Mini Oxygen Tab in a spray bottle lasts around 3 weeks can be used in the kitchen, Bathroom on bench tops etc and has NO perfume.
 
@@ -29,10 +24,12 @@ GREY WATER: - I recommend using the 30 gram tablet can be placed on the far corn
 	{
 		name: '40x8g Cassette Toilet Tablets Box',
 		shortDescription: '',
-		description: `Simply drop 1 x 8 gram Blue tablet into the bottom cassette with half cup water, lasting up to 4 Days usage. The tablet will break down the matter including the paper very quickly. There are 2 size pouches, 50 x 8gram tablets (blue) & 25 x 8gram tablets both include 10 x 2gram. Mini oxygen tabs for use in a spray bottle.
-		The 2 gram Mini Tab (green) is used in a spray bottle, simply drop into a 400 mil bottle with cup hot water & top up with cold water, give it a shake and is ready for use. We use the spray bottle for the bowl & seat, which only takes seconds & in the heat perhaps spray the bowl 3 times a day. By spraying it is killing the bad bacteria & any odour, it is good to open the slide when spraying to allow entry into the bottom cassette.
+		description: `HOLDING TANKS: - Depending on the size of the tank I recommend using 1 x 30 gram tablet (white) for 7 days usage , add more as required. The tablet is similar to the 8 gram but more economical for such tanks.If you have a boat/yacht etc sea water can be used and with a Masurator suggest crush the tablet install pieces to prevent from jamming. If you have the small bottle Mini tabs enclosed in your packaging I suggest use the same method as shown above for the bowl & seat.
 
-		Use any toilet paper & if it is not breaking down use more flushing water.`,
+The Mini Oxygen Tab in a spray bottle lasts around 3 weeks can be used in the kitchen, Bathroom on bench tops etc and has NO perfume.
+
+GREY WATER: - I recommend using the 30 gram tablet can be placed on the far corner of theshower base , this will clean out the pipe lines leading to the tank, also can be placed directly intothe tank.
+		`,
 		specifications: [
 			'A great alternative to your current toilet chemical.',
 			'Small disc that will last up to 7 days in bottom tank',
@@ -49,7 +46,10 @@ GREY WATER: - I recommend using the 30 gram tablet can be placed on the far corn
 	{
 		name: '50x8g RV Toilet Tablets',
 		shortDescription: 'With 10x2g Mini Oxygen Tablets',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim sapien est, vitae egestas eros tempor ut. Curabitur erat sem, consequat eu turpis in, viverra tempor odio. Nunc quis orci suscipit, vulputate nibh in, consectetur felis. Proin commodo ultrices velit, volutpat cursus odio dignissim id. Etiam egestas augue eget quam.',
+		description: `CASSETTE TOILETS : Simply drop 1 x 8 gram Blue tablet into the bottom cassette with half cup water, lasting up to 4 Days usage. The tablet will break down the matter including the paper very quickly. There are 2 size pouches, 50 x 8gram tablets (blue) & 25 x 8gram tablets both include 10 x 2gram. Mini oxygen tabs for use in a spray bottle.
+		The 2 gram Mini Tab (green) is used in a spray bottle, simply drop into a 400 mil bottle with cup hot water & top up with cold water, give it a shake and is ready for use. We use the spray bottle for the bowl & seat, which only takes seconds & in the heat perhaps spray the bowl 3 times a day. By spraying it is killing the bad bacteria & any odour, it is good to open the slide when spraying to allow entry into the bottom cassette.
+
+		Use any toilet paper & if it is not breaking down use more flushing water.`,
 		specifications: [
 			'A great alternative to your current toilet chemical.',
 			'Small disc that will last up to 7 days in bottom tank',
@@ -65,7 +65,10 @@ GREY WATER: - I recommend using the 30 gram tablet can be placed on the far corn
 	{
 		name: '25x8g RV Toilet Tablets',
 		shortDescription: 'With 10x2g Mini Oxygen Tablets',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim sapien est, vitae egestas eros tempor ut. Curabitur erat sem, consequat eu turpis in, viverra tempor odio. Nunc quis orci suscipit, vulputate nibh in, consectetur felis. Proin commodo ultrices velit, volutpat cursus odio dignissim id. Etiam egestas augue eget quam.',
+		description: `CASSETTE TOILETS : Simply drop 1 x 8 gram Blue tablet into the bottom cassette with half cup water, lasting up to 4 Days usage. The tablet will break down the matter including the paper very quickly. There are 2 size pouches, 50 x 8gram tablets (blue) & 25 x 8gram tablets both include 10 x 2gram. Mini oxygen tabs for use in a spray bottle.
+		The 2 gram Mini Tab (green) is used in a spray bottle, simply drop into a 400 mil bottle with cup hot water & top up with cold water, give it a shake and is ready for use. We use the spray bottle for the bowl & seat, which only takes seconds & in the heat perhaps spray the bowl 3 times a day. By spraying it is killing the bad bacteria & any odour, it is good to open the slide when spraying to allow entry into the bottom cassette.
+
+		Use any toilet paper & if it is not breaking down use more flushing water.`,
 		specifications: [
 			'A great alternative to your current toilet chemical.',
 			'Small disc that will last up to 7 days in bottom tank',
@@ -81,7 +84,9 @@ GREY WATER: - I recommend using the 30 gram tablet can be placed on the far corn
 	{
 		name: '10x8g RV Toilet Tablets',
 		shortDescription: '',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim sapien est, vitae egestas eros tempor ut. Curabitur erat sem, consequat eu turpis in, viverra tempor odio. Nunc quis orci suscipit, vulputate nibh in, consectetur felis. Proin commodo ultrices velit, volutpat cursus odio dignissim id. Etiam egestas augue eget quam.',
+		description: `CASSETTE TOILETS : Simply drop 1 x 8 gram Blue tablet into the bottom cassette with half cup water, lasting up to 4 Days usage. The tablet will break down the matter including the paper very quickly.
+
+		Use any toilet paper & if it is not breaking down use more flushing water.`,
 		specifications: [
 			'A great alternative to your current toilet chemical.',
 			'Small disc that will last up to 7 days in bottom tank',
@@ -96,8 +101,9 @@ GREY WATER: - I recommend using the 30 gram tablet can be placed on the far corn
 	},
 	{
 		name: '10x2g Mini Oxygen Tablet Box',
-		shortDescription: 'Box of XX Amount',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim sapien est, vitae egestas eros tempor ut. Curabitur erat sem, consequat eu turpis in, viverra tempor odio. Nunc quis orci suscipit, vulputate nibh in, consectetur felis. Proin commodo ultrices velit, volutpat cursus odio dignissim id. Etiam egestas augue eget quam.',
+		shortDescription: 'Box of 18',
+		description: `Mini oxygen tabs for use in a spray bottle.
+		The 2 gram Mini Tab (green) is used in a spray bottle, simply drop into a 400 mil bottle with cup hot water & top up with cold water, give it a shake and is ready for use. We use the spray bottle for the bowl & seat, which only takes seconds & in the heat perhaps spray the bowl 3 times a day. By spraying it is killing the bad bacteria & any odour, it is good to open the slide when spraying to allow entry into the bottom cassette.`,
 		specifications: [
 			'A great alternative to your current toilet chemical.',
 			'Small disc that will last up to 7 days in bottom tank',
